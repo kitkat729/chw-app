f0eb3e8772a3d15efdc0b4a6f4653374f09349af
Hoi Yuen <hoikityuen@gmail.com>
2018-07-01 18:47:07 -0400

    structure-#1 setup redux store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+import configureStore from './configureStore'
 
 import './App.css';
 
 const App = (props) => {
 
+    const store = configureStore();
+
     return (
       <div className="App">
+        <Provider store={store}>
 
+        </Provider>
       </div>
     )
 }
